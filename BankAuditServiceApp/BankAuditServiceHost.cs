ea6414ccdba8ed6845c6053954078325a25e8fd4
hodoje <nikolakaraklic@gmail.com>
2018-11-07 23:21:39 +0100

    hodoje: Added BankAuditServiceHost class and its implementation. Simple open-close test in Program.cs

---
+++ BankAuditServiceApp/BankAuditServiceHost.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Common.ServiceContracts;
+
+namespace BankAuditServiceApp
+{
+    public class BankAuditServiceHost : IDisposable
+    {
+        private ServiceHost _bankAuditServiceHost;
+        private string _bankAuditServiceAddress;
+        private NetTcpBinding _binding;
+
+        public BankAuditServiceHost()
+        {
+            _bankAuditServiceAddress = ConfigurationManager.AppSettings["bankAuditServiceAddress"];
+            _binding = SetUpBinding();
+            _bankAuditServiceHost = new ServiceHost(typeof(BankAuditService));
+            _bankAuditServiceHost.AddServiceEndpoint(typeof(IBankAuditService), _binding, _bankAuditServiceAddress);
+        }
+
+        private NetTcpBinding SetUpBinding()
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ProtectionLevel = ProtectionLevel.EncryptAndSign;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            return binding;
+        }
+
+        public void OpenService()
+        {
+            try
+            {
+                _bankAuditServiceHost.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"BankAuditServiceHost failed to open with an error: {ex.Message}");
+            }
+        }
+
+        public void CloseService()
+        {
+            try
+            {
+                _bankAuditServiceHost.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"BankAuditServiceHost failed to close with an error: {ex.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            (_bankAuditServiceHost as IDisposable).Dispose();
+        }
+    }
+}
--- BankAuditServiceApp/Program.cs
+++ BankAuditServiceApp/Program.cs
@@ -10,6 +10,11 @@ namespace BankAuditServiceApp
     {
         static void Main(string[] args)
         {
+            BankAuditServiceHost bankAuditServiceHost = new BankAuditServiceHost();
+            bankAuditServiceHost.OpenService();
+            Console.WriteLine("BankAuditService is open...");
+            Console.ReadLine();
+            bankAuditServiceHost.CloseService();
         }
     }
 }
